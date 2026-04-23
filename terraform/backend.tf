d3c5226b37542bc652abaef7f80cb6589eae3719
chris-amaral <chris-amaral@users.noreply.github.com>
2026-04-23 16:41:03 -0300

    fix: resolver pontos da avaliacao - bucket unico, setup automatizado, docs atualizados

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-###############################################################################
-# Backend Configuration (partial)
-# Use: terraform init -backend-config=inventories/<env>/backend.hcl
-# Author: Christopher Amaral
-###############################################################################
-
-terraform {
-  backend "s3" {}
-}
